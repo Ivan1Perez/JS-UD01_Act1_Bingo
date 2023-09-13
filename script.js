f8b33306f3372a69f76e59ad2f616da0bb45725a
Ivan <ivan_psh@hotmail.com>
2023-09-13 13:10:10 +0200

    Acabamos de hacer que saque la bola pulsando espacio sin que se repita la bola

--- script.js
+++ script.js
@@ -1,27 +1,82 @@
-let selectedNumber = "";
+let selectedNumbers = "";
 let randomNum;
-let detector = "";
 
 let cartonJugador = "";
+let lineaCarton = "";
 let cartonMaquina = "";
+let counter = 0;
 
 //CARTON JUGADOR
 console.log("Carton Jugador:");
 
 for (let j = 0; j < 3; j++) {
-  for (let i = 0; i < 5; i++) {
+  do{
     randomNum = Math.floor(Math.random() * 91) + " ";
     /*Mientras que el cartón no incluya uno de los números generados añadimos el nuevo numero*/
-    while (!cartonJugador.includes(randomNum)) {
+    /*Si ya existe ese numero entonces se deberá de volver al Math.random*/
+    if(!cartonJugador.includes(randomNum)) {
       cartonJugador += randomNum;
+      lineaCarton += randomNum;
+      counter++;
     }
-  }
-
-  console.log("-------  " + cartonJugador + "  -------");
-  cartonJugador = "";
+  }while(counter<5);
+    
+  counter = 0;
+  console.log("----- " + lineaCarton + " -----");
+  lineaCarton = "";
 }
 
 console.log(" ");
-console.log(" ");
+
 //CARTON MAQUINA
+
 console.log("Carton Máquina:");
+
+
+for (let j = 0; j < 3; j++) {
+  do{
+    randomNum = Math.floor(Math.random() * 91) + " ";
+    /*Mientras que el cartón no incluya uno de los números generados añadimos el nuevo numero*/
+    /*Si ya existe ese numero entonces se deberá de volver al Math.random*/
+    if(!cartonMaquina.includes(randomNum)) {
+      cartonMaquina += randomNum;
+      lineaCarton += randomNum;
+      counter++;
+    }
+  }while(counter<5);
+    
+  counter = 0;
+  console.log("----- " + lineaCarton + " -----");
+  lineaCarton = "";
+}
+
+
+/*------------------------BOLA-------------------------------*/
+
+let bola;
+let bolasSacadas = "";
+let bolaNoRepetida;
+let bolasCounter = 0;
+
+//Si presionamos la barra espaciadora sacará una bola.
+document.addEventListener('keydown', function(event){
+  if (event.code === "Space") {
+    bolaNoRepetida = false;
+    do{
+      bola = Math.floor(Math.random() * 91);
+      randomNum = bola + " ";
+      if(!(bolasSacadas.includes(randomNum))){
+        bolasSacadas += randomNum;
+        bolaNoRepetida = true;
+      }else{
+        bola = null;
+      }
+    }while(!bolaNoRepetida);
+
+    if(bola!=null){
+      console.log(bola);
+    }
+    
+    event.preventDefault();
+  }
+});
